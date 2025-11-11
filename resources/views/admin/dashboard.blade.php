e28bd4af9b4c8d28e5c53136e388fff899e207d4
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-11-11 16:05:32 +0530

    u

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('header-title', __('Welcome Bacc,') . ' ' . Str::limit(auth()->user()?->name, 20))
+@section('header-title', __('Welcome Back,') . ' ' . Str::limit(auth()->user()?->name, 20))
 @section('header-subtitle', __('Monitor your business analytics and statistics.'))
 
 @section('content')
